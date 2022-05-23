916f872bdee0cb0d1b1bd001798e80684f3a93f5
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2022-05-22 22:40:50 -0300

    updating db with water metas

--- src/Pages/Perfil/index.jsx
+++ src/Pages/Perfil/index.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react';
+import { AuthContext } from '../../context/auth'
+import { getWaterInfo, updateWaterInfo } from '../../services/api';
 
-const index = () => {
+const Profile = () => {
+  const {user} = useContext(AuthContext);
+  const [waterInfo, setWaterInfo] = useState([]);
+
+  //CARREGAR DADOS WATER INFO
+  useEffect(()=> {
+    (async () => await loadData())();
+  }, [])
+  
+  const loadData = async (query = '') => {
+    try{
+      const response = await getWaterInfo(user?.id, query);
+      setWaterInfo(response.data);
+    }catch(err){
+      console.error(err); 
+    }    
+  }
+
+  
+  
+  const mlPerKg = 50;
+  const [ weigth, setWeigth ] = useState(0);  
+  
+  const calcWaterDay = mlPerKg * weigth;
+  const profileMetaWater = (calcWaterDay/1000).toString().substring(0, 4)
+  
+  const handleClick = () => {
+    //enviar pra db
+    updateWaterInfo(user?.id, waterInfo._id , waterInfo.progress, waterInfo.ml, calcWaterDay)
+  }
+
   return (
     <>
     <h1>LEONARDO</h1>
@@ -9,12 +41,12 @@ const index = () => {
       <h2>definir meta agua</h2>
 
       <label htmlFor="">peso em kg</label>
-      <input type="text" />
-      <p>Você deve beber </p>
-      <button></button>
+      <input type="text" value={weigth} onChange={({target}) => setWeigth(target.value)} />
+      <p>Você deve beber: {profileMetaWater}L de água diariamente</p>
+      <button onClick={handleClick}>Definir</button>
     </div>
     </>
   )
 }
 
-export default index
+export default Profile;

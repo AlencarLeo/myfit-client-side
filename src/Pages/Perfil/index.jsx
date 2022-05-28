024e2bd946b48e7c783e37ea1b1306b081dbad0a
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2022-05-28 01:46:20 -0300

    btn update

--- src/Pages/Perfil/index.jsx
+++ src/Pages/Perfil/index.jsx
@@ -1,12 +1,14 @@
 import React, {useState, useEffect, useContext} from 'react';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import {Button} from '../../components/styles/Button.styled'
+import {InputField} from '../../components/styles/InputField.styled'
 import { AuthContext } from '../../context/auth'
 import { getWaterInfo, updateWaterInfo } from '../../services/api';
 
 const Profile = () => {
   const {user} = useContext(AuthContext);
-  const [waterInfo, setWaterInfo] = useState([]);
+  const [waterInfo, setWaterInfo] = useState({});
 
   //CARREGAR DADOS WATER INFO
   useEffect(()=> {
@@ -24,14 +26,14 @@ const Profile = () => {
   
   
   const mlPerKg = 50;
-  const [ weigth, setWeigth ] = useState(0);  
+  const [ weigth, setWeigth ] = useState('');  
   
   const calcWaterDay = mlPerKg * weigth;
   const profileMetaWater = (calcWaterDay/1000).toString().substring(0, 4)
   
-  const handleClick = async () => {    
-    await updateWaterInfo(user?.id, waterInfo._id, waterInfo.ml, calcWaterDay)
-    console.log(waterInfo.progress)
+  const handleClick = async () => { 
+    waterInfo.progress = (100 * waterInfo.ml) / calcWaterDay;
+    await updateWaterInfo(user?.id, waterInfo._id, waterInfo.progress, waterInfo.ml, calcWaterDay);
   }
 
 
@@ -39,15 +41,27 @@ const Profile = () => {
     <>
     <Header />
 
-    <h1>LEONARDO</h1>
+    <h1>Defina suas metas, {user.name}.</h1>
 
     <div>
       <h2>definir meta agua</h2>
 
-      <label htmlFor="">peso em kg</label>
-      <input type="text" value={weigth} onChange={({target}) => setWeigth(target.value)} />
+      <InputField>
+        <input 
+          id='waterMeta'
+          type="number" 
+          placeholder=' '
+          autoComplete='off' 
+          value={weigth} 
+          onChange={({target}) => setWeigth(target.value)} 
+          required
+        />
+        <label htmlFor="waterMeta">peso em kg</label>
+      </InputField>
+
       <p>Você deve beber: {profileMetaWater}L de água diariamente</p>
-      <button onClick={handleClick}>Definir</button>
+      <Button onClick={handleClick}>Definir</Button>
+      
     </div>
 
     <Footer />
--- src/components/styles/Button.styled.js
+++ src/components/styles/Button.styled.js
@@ -1,11 +1,18 @@
 import styled from "styled-components"
 
 export const Button = styled.button`
-  background-color: #00EEC3;
+  background-color: #16CAC7;
   border: none;
   cursor: pointer;
   width: 100%;
   height: 50px;
   border-radius: 5px;
   font-size: 1rem;
+  transition: all .3s ease-in-out;
+  font-weight: 700;
+  text-transform: uppercase;
+
+  &:hover{
+    background-color: #1FFFD6;
+  }
 `
